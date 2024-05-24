473ce306ff850d2f7f78a54e2d284061c39b1666
huanacoCoral <147654342+huanacoCoral@users.noreply.github.com>
2024-05-23 22:06:51 -0400

    Update ejercicio12.c

--- ejercicio12.c
+++ ejercicio12.c
@@ -1,60 +1,39 @@
-#include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <mpi.h>
 
 int fibonacci(int n) {
     if (n <= 1)
         return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
 }
 
-int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
+int main(int argc, char *argv[]) {
+    int rank, size, term, fib_term;
 
-    int rank, size;
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int MASTER = 0;
-    int N = 10; // Número de términos de Fibonacci a calcular
+    int num_terms = size - 1;  // Número de términos a calcular (excluyendo el maestro)
 
-    if (rank == MASTER) {
-        // Procesador MASTER
-        int* fib_series = (int*)malloc(N * sizeof(int));
-        int term;
-        
-        for (int i = 0; i < N; i++) {
-            int worker = (i % (size - 1)) + 1; // Distribuye trabajos entre los SLAVES
-            MPI_Send(&i, 1, MPI_INT, worker, 0, MPI_COMM_WORLD);
-            MPI_Recv(&term, 1, MPI_INT, worker, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            fib_series[i] = term;
+    if (rank == 0) {
+        // Proceso maestro
+        for (int i = 1; i <= num_terms; i++) {
+            // Solicita término de Fibonacci al esclavo i
+            MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
 
-        printf("Serie de Fibonacci: ");
-        for (int i = 0; i < N; i++) {
-            printf("%d ", fib_series[i]);
+        // Recibe y muestra los términos de Fibonacci de los esclavos
+        for (int i = 1; i <= num_terms; i++) {
+            MPI_Recv(&fib_term, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Fibonacci(%d) = %d\n", i, fib_term);
         }
-        printf("\n");
-
-        free(fib_series);
-
-        // Indicar a los esclavos que finalicen
-        term = -1;
-        for (int i = 1; i < size; i++) {
-            MPI_Send(&term, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-
     } else {
-        // Procesadores SLAVE
-        int n;
-        while (1) {
-            MPI_Recv(&n, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if (n == -1) {
-                break; // Terminar el proceso si recibe la señal de finalización
-            }
-            int result = fibonacci(n);
-            MPI_Send(&result, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
-        }
+        // Procesos esclavos
+        MPI_Recv(&term, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        fib_term = fibonacci(term);
+        MPI_Send(&fib_term, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); // Enviar el término calculado al proceso maestro
     }
 
     MPI_Finalize();
